4d8ecf466f6557f3b25ae04dc4b65687c55458ab
Sebastian Rios <juanriosubeda@gmail.com>
2025-12-18 15:06:36 -0800

    Exercise #10 Done (Refactor of Exercise #9)

--- components/ErrorHandling.tsx
+++ components/ErrorHandling.tsx
@@ -4,17 +4,12 @@ import { Loader2 } from "lucide-react";
 const timeout = 1000;
 
 const ErrorHandling = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [data, setData] = useState<string | undefined>();
-  const [error, setError] = useState(false);
-  const [idle, setIdle] = useState(true);
+  type Status = "idle" | "Loading" | "Success" | "Error";
+  const [status, setStatus] = useState<Status>("idle");
 
   const handleRequest = () => {
     //1. Reset the UI to a Fresh 'Loading State'
-    setIsLoading(true);
-    setError(false);
-    setData(undefined);
-    setIdle(false);
+    setStatus("Loading");
 
     setTimeout(() => {
       // 2. Decide the outcome LOCALLY first
@@ -23,14 +18,10 @@ const ErrorHandling = () => {
 
       //3. Update the state based on the outcome
       if (randomOutcome) {
-        setData("Success");
-        setError(false);
+        setStatus("Success");
       } else {
-        setData(undefined);
-        setError(true);
+        setStatus("Error");
       }
-      //4. Finalize the loading state
-      setIsLoading(false);
     }, timeout);
   };
 
@@ -39,28 +30,18 @@ const ErrorHandling = () => {
       <div className="flex flex-col items-center justify-center gap-4">
         <p className="mt-4">Error Handling</p>
         <button
-          disabled={isLoading}
+          disabled={status === "Loading"}
           onClick={handleRequest}
           className={`px-4 py-2 rounded-lg bg-zinc-900 text-white dark:bg-zinc-100 dark:text-zinc-900 transition-colors hover:bg-zinc-400 active:scale-95 shadow-sm  flex gap-1 items-center justify-center ${
-            isLoading && "opacity-50 cursor-not-allowed"
+            status === "Loading" && "opacity-50 cursor-not-allowed"
           }`}
         >
           Fetch Data
         </button>
-        {isLoading ? (
-          // <p>Loading...</p>
-          <Loader2 className="animate-spin mt-4" />
-        ) : (
-          !idle && (
-            <div>
-              {error ? (
-                <p className="text-red-500">Error in fetching the Data</p>
-              ) : (
-                <p className="text-green-500">Success in fetching the Data</p>
-              )}
-            </div>
-          )
-        )}
+
+        {status === "Loading" && <Loader2 className="animate-spin mt-4" />}
+        {status === "Success" && <p className="text-green-500">Success in fetching the Data</p>}
+        {status === "Error" && <p className="text-red-500">Error in fetching the Data</p>}
       </div>
     </div>
   );
